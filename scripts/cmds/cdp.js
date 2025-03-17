9d28e0b3f2b93287c8a296cc4d9955015d310ad0
remdil <167093544+remdil45@users.noreply.github.com>
2025-03-17 23:28:22 +0530

    Update cdp.js

--- scripts/cmds/cdp.js
+++ scripts/cmds/cdp.js
@@ -2,47 +2,50 @@ const axios = require("axios");
 const fs = require("fs-extra");
 
 module.exports = {
- config: {
- name: "cdp",
- aliases: [],
- version: "1.0",
- author: "XyryllPanget",
- countDown: 5,
- role: 0,
- shortDescription: {
- en: "couple dp"
- },
- longDescription: {
- en: "couple dp"
- },
- category: "love",
- guide: {
- en: "{pn}"
- }
- },
+    config: {
+        name: "cdp",
+        aliases: ["cdp", "pairdp"],
+        version: "2.0",
+        author: "RUBISH",
+        countDown: 5,
+        role: 0,
+        shortDescription: {
+            en: "Random Couple dp"
+        },
+        longDescription: {
+            en: "Random Couple dp"
+        },
+        category: "image",
+        guide: {
+            en: "{pn}"
+        }
+    },
 
- onStart: async function ({ api, event, args }) {
- try {
- const { data } = await axios.get(
- "https://tanjiro-api.onrender.com/cdp?api_key=tanjiro"
- );
- const maleImg = await axios.get(data.male, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img1.png", Buffer.from(maleImg.data, "utf-8"));
- const femaleImg = await axios.get(data.female, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img2.png", Buffer.from(femaleImg.data, "utf-8"));
+    onStart: async function ({ api, event, args }) {
+        try {
+            const response = await axios.get("https://rubish-apihub.onrender.com/rubish//cdp?&apikey=rubish69");
+            const data = response.data;
 
- const msg = "「 Here's your pair Dp✨ 」";
- const allImages = [
- fs.createReadStream(__dirname + "/tmp/img1.png"),
- fs.createReadStream(__dirname + "/tmp/img2.png")
- ];
+            const maleImgResponse = await axios.get(data.images[0], { responseType: "arraybuffer" });
+            fs.writeFileSync(__dirname + "/tmp/img1.png", Buffer.from(maleImgResponse.data, "utf-8"));
+            const femaleImgResponse = await axios.get(data.images[1], { responseType: "arraybuffer" });
+            fs.writeFileSync(__dirname + "/tmp/img2.png", Buffer.from(femaleImgResponse.data, "utf-8"));
 
- return api.sendMessage({
- body: msg,
- attachment: allImages
- }, event.threadID, event.messageID);
- } catch (error) {
- console.error(error);
- }
- }
+            const msg = `
+✅ | Here is your couple dp 
+
+⦿ ID: ${data.serialNumber}`;
+            const allImages = [
+                fs.createReadStream(__dirname + "/tmp/img1.png"),
+                fs.createReadStream(__dirname + "/tmp/img2.png")
+            ];
+
+            await api.sendMessage({
+                body: msg,
+                attachment: allImages
+            }, event.threadID, event.messageID);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 };
